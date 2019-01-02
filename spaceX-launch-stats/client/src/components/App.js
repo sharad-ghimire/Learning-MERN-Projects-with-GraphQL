00ae12d3e37d4093813b6f5dad057cd594f2525b
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-01-02 21:42:17 +1100

    React Client init [SpaceX-Stats]

---
+++ spaceX-launch-stats/client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/SpaceX-Logo.svg/2000px-SpaceX-Logo.svg.png"
+        alt="SpacreX"
+        height="45"
+        width="200"
+      />
+    </div>
+  );
+};
+export default App;
